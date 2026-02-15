e1f5c6f8557150983ffe27854b14e3c4e64af20e
Dominik Josipovic <dominik.josipovic@gmail.com>
2026-02-15 11:37:22 +0100

    initial login component

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,10 +2,20 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { providePrimeNG } from 'primeng/config';
+import Lara from '@primeuix/themes/lara';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAnimations(),
+    providePrimeNG({
+      ripple: true,
+      theme: {
+        preset: Lara
+      }
+    })
   ]
 };

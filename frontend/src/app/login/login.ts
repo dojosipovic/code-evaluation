330a7aaabc97e121d1c29d0f8f958a87dc5af443
Dominik Josipovic <dominik.josipovic@gmail.com>
2026-02-15 12:10:09 +0100

    initial login UI

--- frontend/src/app/login/login.ts
+++ frontend/src/app/login/login.ts
@@ -1,11 +1,55 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+import { DividerModule } from 'primeng/divider';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    CardModule,
+    InputTextModule,
+    PasswordModule,
+    ButtonModule,
+    CheckboxModule,
+    DividerModule
+  ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+  loading = false;
+  form: any;
 
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      remember: [true]
+    })
+  }
+
+  
+
+  isInvalid(name: 'username' | 'password') {
+    const c = this.form.get(name);
+    return !!c && c.invalid && (c.dirty || c.touched);
+  }
+
+  submit() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    // za sada samo demo
+    console.log('login form', this.form.getRawValue());
+  }
 }
